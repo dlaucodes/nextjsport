658295d90482f2920548a783520f9bec504a148c
Chun Lau <dlaucodes@gmail.com>
2024-01-26 21:52:07 -0500

    add framer motion

---
+++ components/sub/HeroContent.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {motion} from 'framer-motion'
+
+export const HeroContent = () => {
+  return (
+    <motion.div
+        initial = "hidden"
+        animate="visible"
+        className='flex flex-row items-center justift-center px-20 mt-40 w-full z-[20]'>
+            <div className="h-full w-full flex flex-col gap-5 justifu-center m-auto text-start">
+                <motion.div
+                    className="Welcome-box py-[15px] px-[4px] border border -[#7042f88b] opacity=[0.9]">
+                        Hello
+                    </motion.div>
+            </div>
+        </motion.div>
+  )
+}
+
